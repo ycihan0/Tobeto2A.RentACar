a21ba39dcf31c2cd160a3eaf7dbe00b85f1aa911
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-03-04 17:13:49 +0300

    Auth

--- src/tobeto2A.RentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
+++ src/tobeto2A.RentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
 using Domain.Entities;
 using NArchitecture.Core.Application.Rules;
 using NArchitecture.Core.CrossCuttingConcerns.Exception.Types;
@@ -21,6 +22,6 @@ public class BrandBusinessRules : BaseBusinessRules
     {
         Brand? brandWithSameName = await _brandRepository.GetAsync(b => b.Name == name);
         if (brandWithSameName is not null)
-            throw new BusinessException("Aynı isme sahip bir marka zaten mevcuttur");
+            throw new BusinessException(BrandsMessages.BrandAlreadyExist);
     }
 }
